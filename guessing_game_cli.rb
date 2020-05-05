1fb9ec1c86158cd59e628e2996e1c2f342f7dd83
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-05-05 19:24:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,5 @@
 def run_guessing_game
   random_number = rand(1..6)
-  puts "Guess a number between 1 and 6."
   input = gets.chomp
   if input.to_i == random_number
     puts "You guessed the correct number!"
